3e261e28641050df196ddc028c1199e1080f663d
Thejeswar <64600548+THEJESWAR21@users.noreply.github.com>
2021-02-28 21:25:22 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+//default App
+export default function App(){
+  
+  return(
+    <View style={styles.container}>
+      -
+    </View>
+  )
+  }
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    color: '#fff',
+  },
+})
